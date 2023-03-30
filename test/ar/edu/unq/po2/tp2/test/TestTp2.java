3f9be94338d75a0e59add3145192ae0a72a92abe
Tomas Fuentes <tomasrfuentes@gmail.com>
2023-03-29 23:41:00 -0300

    Planeamiento de tests

--- test/ar/edu/unq/po2/tp2/test/TestTp2.java
+++ test/ar/edu/unq/po2/tp2/test/TestTp2.java
@@ -2,5 +2,20 @@ package ar.edu.unq.po2.tp2.test;
 import ar.edu.unq.po2.tp2.*;
 
 public class TestTp2 {
-
+    /**
+     Tests pendientes:
+     
+     *Empresa
+     *Empleado
+     *SubclasesEmpleado
+     *Recibos
+     
+     Clases de testeo:
+     
+     *Empresa
+     *empleado planta perm
+     *empleado planta temp
+     
+     * 
+     */
 }

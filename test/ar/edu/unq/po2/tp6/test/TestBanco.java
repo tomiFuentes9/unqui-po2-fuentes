e9003cba02a9503ac15c46945e1206c0cd6fd46d
Tomas Fuentes <tomasrfuentes@gmail.com>
2023-04-25 23:21:53 -0300

    TP 6 planeamiento TDD

---
+++ test/ar/edu/unq/po2/tp6/test/TestBanco.java
@@ -0,0 +1,50 @@
+package ar.edu.unq.po2.tp6.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.*; preguntar como instalar mockito
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestBanco {
+	/*private Banco viellesuper;
+	private Cliente pam;
+	private Cliente jim;
+	private Cliente dwight;
+	*/
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		//viellesuper = new Banco() Cambiar por constructor
+		//pam = mock(Cliente.class)
+		//jim = mock(Cliente.class)
+		//dwight = mock(Cliente.class)
+	}
+
+	@Test
+	void testConClientes() {
+		fail("Not yet implemented");
+	}
+	
+	void testRecibirSolicitudCredito() {
+		fail("Not yet implemented");
+	}
+	
+	void testRecibirSolicitudCreditoPersonal() {
+		fail("Not yet implemented");
+	}
+	
+	void testRecibirSolicitudCreditoHipotecario() {
+		fail("Not yet implemented");
+	}
+	
+	void testSolicitudAceptada() {
+		fail("Not yet implemented");
+	}
+	
+	void testSolicitudRechazada() {
+		fail("Not yet implemented");
+	}
+
+}
